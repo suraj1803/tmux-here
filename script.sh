a6b7fb5845ddb760f1760f7d49d52854ed158c39
Suraj Biswas <surajbiswas182003@gmail.com>
2026-09-24 02:59:53 +0530

    feat: add tmux ls with fzf

--- script.sh
+++ script.sh
@@ -10,9 +10,20 @@ tm() {
     else
         if [[ "$1" == "ls" ]]; then
             local tmux_sessions=$(tmux ls 2>/dev/null | awk -F: '{print $1}')
+
+            if [[ "$tmux_sessions" == "" ]]; then
+                echo "No tmux sessions found."
+                return 1
+            fi
+
             local selected_session=$(echo "$tmux_sessions" | fzf --prompt="Select tmux session> " --height=40% --reverse)
 
             [[ -z "$selected_session" ]] && return 0
+
+            if [[ "$selected_session" == ":q" ]]; then
+                return 0
+            fi
+
 
             if [[ -n $TMUX ]]; then
                 tmux switch-client -t "$selected_session"
